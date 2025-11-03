fbc6acd6f435989f8bc358f829027dfb18f47577
WebProgrammingKrisnaPutra <136696090+WebProgrammingKrisnaPutra@users.noreply.github.com>
2025-11-04 02:27:11 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+    // === NAVBAR MENU TOGGLE ===
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.getElementById('nav-menu').querySelector('ul');
+
+    menuToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('show');
+    });
+
+        
